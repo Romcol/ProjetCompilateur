1a40e48d9cb985a3dea296d01316540def3ad727
Romain Colombat <romcol85@gmail.com>
2015-03-05 17:50:23 +0100

    add Ident

--- src/TabIdent.java
+++ src/TabIdent.java
@@ -1,4 +1,13 @@
+import java.util.HashMap;
 
 public class TabIdent {
-
+	private HashMap<String,Ident> table;
+	public TabIdent(int taille) {
+	}
+	public Ident chercheIdent(String clef{
+	}
+	public boolean existeIdent(String clef){
+	}
+	public void rangeIdent(String clef, Ident id){
+	}
 }
