0c94ffb8758003baf9392167514b9e9afc10c735
Guillaume Perrudin <guillaume.perrudin@insa-rennes.fr>
2015-03-09 14:29:38 +0100

    Implementation of the variables stack + error metod (YVM.java)

--- src/YVM.java
+++ src/YVM.java
@@ -1,4 +1,27 @@
+import java.util.Stack;
+
 
 public class YVM {
+
+	protected Stack<IdVar> pile;
+	
+	
+	public YVM() {
+		this.pile = new Stack<IdVar>();
+	}
+
+	public void addVar(IdVar var) {
+		this.pile.push(var);
+	}
+
+	public void getVar(int offset) {
+		this.pile.get(-2 * offset);
+	}
+	
+	
+	
+	public static void error(String message) {
+		System.out.println(message + "\n");
+	}
 
 }
