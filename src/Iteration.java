d5f4539491d86b30b7939e573a36cacd6a129701
Romain Colombat <romcol85@gmail.com>
2015-04-01 11:50:55 +0200

    Iteration

---
+++ src/Iteration.java
@@ -0,0 +1,34 @@
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+
+public class Iteration implements Constants{
+	private int itIdent = 0;
+	private Stack<Integer> pileIt= new Stack<Integer>();
+	
+	public void push()
+	{
+		itIdent++;
+		pileIt.push(itIdent);
+		
+		Yaka.yvm.beginIt(itIdent);
+	}
+	
+	public void close (){
+		try{
+			int id = pileIt.pop();
+			Yaka.yvm.closeIt(id);
+		}
+		catch(EmptyStackException e) {
+			Yaka.yvm.ecrireErreur("Unexpected end of iteration");
+		}
+
+	}
+	
+	public void checkExprType (ValueType type)
+	{
+		if (type != ValueType.BOOLEEN){
+			Yaka.yvm.ecrireErreur("Expected boolean expression in TANTQUE");
+		}
+	}
+}
