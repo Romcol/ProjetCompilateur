a5888b4d76d01a2f3456505a98192a060f035450
Romain Colombat <romcol85@gmail.com>
2015-04-20 15:22:29 +0200

    IdFunct

---
+++ src/IdFunct.java
@@ -0,0 +1,18 @@
+
+public class IdFunct extends Ident{
+
+	public IdFunct(){
+		
+	}
+	
+	public void load() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void store() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/TabIdent.java
+++ src/TabIdent.java
@@ -5,27 +5,28 @@ public class TabIdent {
 	
 	private int nbVar;
 	
-
-	private HashMap<String,Ident> table;
+	private HashMap<String,Ident> table_globaux;
+	private HashMap<String,Ident> table_locaux;
 	
 	public TabIdent() {
 		this.nbVar = 0;
-		this.table = new HashMap<String, Ident>();
+		this.table_locaux = new HashMap<String, Ident>();
+		this.table_globaux = new HashMap<String, Ident>();
 	}
 	public Ident chercheIdent(String clef){
-		return table.get(clef);
+		return table_locaux.get(clef);
 	}
 	public boolean existeIdent(String clef){
-		return table.containsKey(clef);
+		return table_locaux.containsKey(clef);
 	}
 	
 	public void rangeIdent(String clef, IdConst id){
-		table.put(clef, id);
+		table_locaux.put(clef, id);
 	}
 	
 	public void rangeIdent(String clef, IdVar id){
 		id.setValue((nbVar+1)*(-2));
-		table.put(clef, id);
+		table_locaux.put(clef, id);
 		this.nbVar++;
 
 	}
@@ -34,15 +35,15 @@ public class TabIdent {
 		
 		String ret = "";
 		
-		for (Map.Entry<String, Ident> entry : table.entrySet())
+		for (Map.Entry<String, Ident> entry : table_locaux.entrySet())
 		{
 				ret += "(" + entry.getKey() + " : " + entry.getValue() + ")\n";
 		}
 		return ret;
 	}
 	
-	public HashMap<String, Ident> getTable() {
-		return table;
+	public HashMap<String, Ident> gettable_locaux() {
+		return table_locaux;
 	}
 
 	public int getNbVar() {
