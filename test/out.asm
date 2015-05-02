557a7880c881e0980437561f38f5e5a16f546c83
Romain Colombat <romcol85@gmail.com>
2015-05-02 18:16:18 +0200

    Nouveau fichier prog.yaka

--- test/out.asm
+++ test/out.asm
@@ -6,32 +6,24 @@
 .model SMALL
 .386
 .CODE
-	debut :
-	STARTUPCODE
 
-	;ouvrePrinc 8
-	mov bp, sp
-	sub sp,8
+max:
 
-	;iconst 1
-	push word ptr 1
+	;ouvbloc 6
+	enter 6,0
 
-	;istore -4
-	pop ax
-	mov word ptr [bp-4],ax
-
-	;iconst 2
-	push word ptr 2
+	;iload 6
+	push word ptr [bp+6]
 
 	;istore -6
 	pop ax
 	mov word ptr [bp-6],ax
 
 	;iload -6
-	push word ptr [bp-6]
+	push word ptr [bp+-6]
 
-	;iload -4
-	push word ptr [bp-4]
+	;iload 4
+	push word ptr [bp+4]
 
 	;isup
 	pop bx
@@ -42,119 +34,201 @@
 	jmp $+4
 	push 0
 
-	;iffaux SINON1:
+	;iffaux SINON1
 	pop ax
 	cmp ax, 0
 	je SINON1
 
-	;ecrireChaine "OK"
-.DATA
-	mess0 DB "OK$"
-.CODE
-	lea dx, mess0
-	push dx
-	call ecrch
-
 	;iload -6
-	push word ptr [bp-6]
+	push word ptr [bp+-6]
 
-	;iload -4
-	push word ptr [bp-4]
-
-	;iinf
-	pop bx
+	;ireturn 8
 	pop ax
-	cmp ax,bx
-	jge $+6
-	push -1
-	jmp $+4
-	push 0
-
-	;iffaux SINON2:
-	pop ax
-	cmp ax, 0
-	je SINON2
-
-	;ecrireChaine "POK"
-.DATA
-	mess1 DB "POK$"
-.CODE
-	lea dx, mess1
-	push dx
-	call ecrch
-
-	;goto FSI2
-	jmp FSI2
-
-SINON2:
-
-	;ecrireChaine "OK"
-.DATA
-	mess2 DB "OK$"
-.CODE
-	lea dx, mess2
-	push dx
-	call ecrch
-
-	;iload -6
-	push word ptr [bp-6]
-
-	;iload -4
-	push word ptr [bp-4]
-
-	;iinf
-	pop bx
-	pop ax
-	cmp ax,bx
-	jge $+6
-	push -1
-	jmp $+4
-	push 0
-
-	;iffaux SINON3:
-	pop ax
-	cmp ax, 0
-	je SINON3
-
-	;ecrireChaine "POK"
-.DATA
-	mess3 DB "POK$"
-.CODE
-	lea dx, mess3
-	push dx
-	call ecrch
-
-	;goto FSI3
-	jmp FSI3
-
-SINON3:
-
-	;ecrireChaine "OK"
-.DATA
-	mess4 DB "OK$"
-.CODE
-	lea dx, mess4
-	push dx
-	call ecrch
-
-FSI3:
-
-FSI2:
+	mov [bp+8],ax
 
 	;goto FSI1
 	jmp FSI1
 
 SINON1:
 
-	;ecrireChaine "POK"
-.DATA
-	mess5 DB "POK$"
-.CODE
-	lea dx, mess5
-	push dx
-	call ecrch
+	;iload 4
+	push word ptr [bp+4]
+
+	;ireturn 8
+	pop ax
+	mov [bp+8],ax
 
 FSI1:
+
+	;fermebloc 4
+	leave
+	ret 4
+
+min:
+
+	;ouvbloc 0
+	enter 0,0
+
+	;iload 6
+	push word ptr [bp+6]
+
+	;iload 4
+	push word ptr [bp+4]
+
+	;iinf
+	pop bx
+	pop ax
+	cmp ax,bx
+	jge $+6
+	push -1
+	jmp $+4
+	push 0
+
+	;iffaux SINON2
+	pop ax
+	cmp ax, 0
+	je SINON2
+
+	;iload 6
+	push word ptr [bp+6]
+
+	;ireturn 8
+	pop ax
+	mov [bp+8],ax
+
+	;goto FSI2
+	jmp FSI2
+
+SINON2:
+
+	;iload 4
+	push word ptr [bp+4]
+
+	;ireturn 8
+	pop ax
+	mov [bp+8],ax
+
+FSI2:
+
+	;fermebloc 4
+	leave
+	ret 4
+
+sup:
+
+	;ouvbloc 0
+	enter 0,0
+
+	;iload 6
+	push word ptr [bp+6]
+
+	;iload 4
+	push word ptr [bp+4]
+
+	;isup
+	pop bx
+	pop ax
+	cmp ax,bx
+	jle $+6
+	push -1
+	jmp $+4
+	push 0
+
+	;ireturn 8
+	pop ax
+	mov [bp+8],ax
+
+	;fermebloc 4
+	leave
+	ret 4
+
+	;entete main
+	debut :
+	STARTUPCODE
+	main :
+
+	;ouvbloc 8
+	enter 8,0
+
+	;iconst 5
+	push word ptr 5
+
+	;lireEnt -4
+	lea dx, [bp-4]
+	push dx
+	call lirent
+
+	;aLaLigne
+	call ligsuiv
+
+	;iload -2
+	push word ptr [bp+-2]
+
+	;iload -4
+	push word ptr [bp+-4]
+
+	;iconst 5
+	push word ptr 5
+
+	;iconst 2
+	push word ptr 2
+
+	;iadd
+	pop bx
+	pop ax
+	add ax,bx
+	push ax
+
+	;iconst 1
+	push word ptr 1
+
+	;iload -2
+	push word ptr [bp+-2]
+
+	;iload -4
+	push word ptr [bp+-4]
+
+	;iconst 5
+	push word ptr 5
+
+	;isub
+	pop bx
+	pop ax
+	sub ax,bx
+	push ax
+
+	;iadd
+	pop bx
+	pop ax
+	add ax,bx
+	push ax
+
+	;iload -2
+	push word ptr [bp+-2]
+
+	;iconst 2
+	push word ptr 2
+
+	;imul
+	pop bx
+	pop ax
+	imul bx
+	push ax
+
+	;iload -4
+	push word ptr [bp+-4]
+
+	;aLaLigne
+	call ligsuiv
+
+	;iload -6
+	push word ptr [bp+-6]
+
+	;aLaLigne
+	call ligsuiv
+
+	;iload -8
+	push word ptr [bp+-8]
 
 	;queue
 	nop
