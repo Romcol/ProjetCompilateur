0228b3d110369135f7734c26105306217c73adee
Guillaume Perrudin <guillaume.perrudin@insa-rennes.fr>
2015-05-11 11:12:27 +0200

    tests jour j

--- test/asm/out.asm
+++ test/asm/out.asm
@@ -7,29 +7,78 @@
 .386
 .CODE
 
-max:
+somdiv:
 
-	;ouvbloc 6
-	enter 6,0
+	;ouvbloc 4
+	enter 4,0
 
-	;iload 6
-	push word ptr [bp+6]
+	;iconst 1
+	push word ptr 1
 
-	;istore -6
+	;istore -4
 	pop ax
-	mov word ptr [bp-6],ax
+	mov word ptr [bp-4],ax
 
-	;iload -6
-	push word ptr [bp-6]
+	;iconst 2
+	push word ptr 2
+
+	;istore -2
+	pop ax
+	mov word ptr [bp-2],ax
+
+FAIRE1:
+
+	;iload -2
+	push word ptr [bp-2]
 
 	;iload 4
 	push word ptr [bp+4]
 
-	;isup
+	;iconst 2
+	push word ptr 2
+
+	;idiv
+	pop bx
+	pop ax
+	cwd
+	idiv bx
+	push ax
+
+	;iffaux FAIT1
+	pop ax
+	cmp ax, 0
+	je FAIT1
+
+	;iload 4
+	push word ptr [bp+4]
+
+	;iload -2
+	push word ptr [bp-2]
+
+	;idiv
+	pop bx
+	pop ax
+	cwd
+	idiv bx
+	push ax
+
+	;iload -2
+	push word ptr [bp-2]
+
+	;imul
+	pop bx
+	pop ax
+	imul bx
+	push ax
+
+	;iload 4
+	push word ptr [bp+4]
+
+	;iegal
 	pop bx
 	pop ax
 	cmp ax,bx
-	jle $+6
+	jne $+6
 	push -1
 	jmp $+4
 	push 0
@@ -39,47 +88,179 @@ max:
 	cmp ax, 0
 	je SINON1
 
-	;iload -6
-	push word ptr [bp-6]
+	;iload -4
+	push word ptr [bp-4]
 
-	;ireturn 8
+	;iload -2
+	push word ptr [bp-2]
+
+	;iadd
+	pop bx
 	pop ax
-	mov [bp+8],ax
+	add ax,bx
+	push ax
+
+	;istore -4
+	pop ax
+	mov word ptr [bp-4],ax
 
 	;goto FSI1
 	jmp FSI1
 
 SINON1:
 
-	;iload 4
-	push word ptr [bp+4]
-
-	;ireturn 8
-	pop ax
-	mov [bp+8],ax
-
 FSI1:
 
-	;fermebloc 4
+	;iload -2
+	push word ptr [bp-2]
+
+	;iconst 1
+	push word ptr 1
+
+	;iadd
+	pop bx
+	pop ax
+	add ax,bx
+	push ax
+
+	;istore -2
+	pop ax
+	mov word ptr [bp-2],ax
+
+	;goto FAIRE1
+	jmp FAIRE1
+
+FAIT1:
+
+	;iload -4
+	push word ptr [bp-4]
+
+	;ireturn 6
+	pop ax
+	mov [bp+6],ax
+
+	;fermebloc 2
 	leave
-	ret 4
+	ret 2
 
-min:
+	;entete main
+	debut :
+	STARTUPCODE
+	main :
 
-	;ouvbloc 0
-	enter 0,0
+	;ouvbloc 10
+	enter 10,0
 
-	;iload 6
-	push word ptr [bp+6]
+	;iconst 1
+	push word ptr 1
 
-	;iload 4
-	push word ptr [bp+4]
+	;istore -2
+	pop ax
+	mov word ptr [bp-2],ax
 
-	;iinf
+FAIRE2:
+
+	;iload -2
+	push word ptr [bp-2]
+
+	;iconst 300
+	push word ptr 300
+
+	;iinfegal
 	pop bx
 	pop ax
 	cmp ax,bx
-	jge $+6
+	jg $+6
+	push -1
+	jmp $+4
+	push 0
+
+	;iffaux FAIT2
+	pop ax
+	cmp ax, 0
+	je FAIT2
+
+	;iload -2
+	push word ptr [bp-2]
+
+	;istore -4
+	pop ax
+	mov word ptr [bp-4],ax
+
+FAIRE3:
+
+	;iload -4
+	push word ptr [bp-4]
+
+	;iconst 300
+	push word ptr 300
+
+	;iinfegal
+	pop bx
+	pop ax
+	cmp ax,bx
+	jg $+6
+	push -1
+	jmp $+4
+	push 0
+
+	;iffaux FAIT3
+	pop ax
+	cmp ax, 0
+	je FAIT3
+
+	;reserveRetour
+	sub sp,2
+
+	;iload -2
+	push word ptr [bp-2]
+
+	;call somdiv
+	call somdiv
+
+	;istore -6
+	pop ax
+	mov word ptr [bp-6],ax
+
+	;reserveRetour
+	sub sp,2
+
+	;iload -4
+	push word ptr [bp-4]
+
+	;call somdiv
+	call somdiv
+
+	;istore -8
+	pop ax
+	mov word ptr [bp-8],ax
+
+	;iload -6
+	push word ptr [bp-6]
+
+	;iload -4
+	push word ptr [bp-4]
+
+	;iegal
+	pop bx
+	pop ax
+	cmp ax,bx
+	jne $+6
+	push -1
+	jmp $+4
+	push 0
+
+	;iload -8
+	push word ptr [bp-8]
+
+	;iload -2
+	push word ptr [bp-2]
+
+	;iegal
+	pop bx
+	pop ax
+	cmp ax,bx
+	jne $+6
 	push -1
 	jmp $+4
 	push 0
@@ -89,184 +270,22 @@ min:
 	cmp ax, 0
 	je SINON2
 
-	;iload 6
-	push word ptr [bp+6]
-
-	;ireturn 8
-	pop ax
-	mov [bp+8],ax
-
-	;goto FSI2
-	jmp FSI2
-
-SINON2:
-
-	;iload 4
-	push word ptr [bp+4]
-
-	;ireturn 8
-	pop ax
-	mov [bp+8],ax
-
-FSI2:
-
-	;fermebloc 4
-	leave
-	ret 4
-
-sup:
-
-	;ouvbloc 0
-	enter 0,0
-
-	;iload 6
-	push word ptr [bp+6]
-
-	;iload 4
-	push word ptr [bp+4]
-
-	;isup
-	pop bx
-	pop ax
-	cmp ax,bx
-	jle $+6
-	push -1
-	jmp $+4
-	push 0
-
-	;ireturn 8
-	pop ax
-	mov [bp+8],ax
-
-	;fermebloc 4
-	leave
-	ret 4
-
-	;entete main
-	debut :
-	STARTUPCODE
-	main :
-
-	;ouvbloc 8
-	enter 8,0
-
-	;iconst 5
-	push word ptr 5
-
-	;istore -2
-	pop ax
-	mov word ptr [bp-2],ax
-
-	;iconst 7
-	push word ptr 7
-
-	;istore -4
-	pop ax
-	mov word ptr [bp-4],ax
-
-	;aLaLigne
-	call ligsuiv
-
-	;reserveRetour
-	sub sp,2
-
 	;iload -2
 	push word ptr [bp-2]
 
-	;reserveRetour
-	sub sp,2
+	;ecrireEnt
+	call ecrent
+
+	;ecrireChaine "--"
+.DATA
+	mess0 DB "--$"
+.CODE
+	lea dx, mess0
+	push dx
+	call ecrch
 
 	;iload -4
 	push word ptr [bp-4]
-
-	;iconst 5
-	push word ptr 5
-
-	;call min
-	call min
-
-	;call max
-	call max
-
-	;iconst 2
-	push word ptr 2
-
-	;iadd
-	pop bx
-	pop ax
-	add ax,bx
-	push ax
-
-	;istore -6
-	pop ax
-	mov word ptr [bp-6],ax
-
-	;reserveRetour
-	sub sp,2
-
-	;iconst 1
-	push word ptr 1
-
-	;reserveRetour
-	sub sp,2
-
-	;iload -2
-	push word ptr [bp-2]
-
-	;iload -4
-	push word ptr [bp-4]
-
-	;iconst 5
-	push word ptr 5
-
-	;isub
-	pop bx
-	pop ax
-	sub ax,bx
-	push ax
-
-	;call max
-	call max
-
-	;iadd
-	pop bx
-	pop ax
-	add ax,bx
-	push ax
-
-	;reserveRetour
-	sub sp,2
-
-	;iload -2
-	push word ptr [bp-2]
-
-	;iconst 2
-	push word ptr 2
-
-	;imul
-	pop bx
-	pop ax
-	imul bx
-	push ax
-
-	;iload -4
-	push word ptr [bp-4]
-
-	;call min
-	call min
-
-	;call sup
-	call sup
-
-	;istore -8
-	pop ax
-	mov word ptr [bp-8],ax
-
-	;aLaLigne
-	call ligsuiv
-
-	;iload -6
-	push word ptr [bp-6]
 
 	;ecrireEnt
 	call ecrent
@@ -274,11 +293,59 @@ sup:
 	;aLaLigne
 	call ligsuiv
 
-	;iload -8
-	push word ptr [bp-8]
+	;lireEnt -10
+	lea dx, [bp-10]
+	push dx
+	call lirent
 
-	;ecrireBool
-	call ecrbool
+	;goto FSI2
+	jmp FSI2
+
+SINON2:
+
+FSI2:
+
+	;iload -4
+	push word ptr [bp-4]
+
+	;iconst 1
+	push word ptr 1
+
+	;iadd
+	pop bx
+	pop ax
+	add ax,bx
+	push ax
+
+	;istore -4
+	pop ax
+	mov word ptr [bp-4],ax
+
+	;goto FAIRE3
+	jmp FAIRE3
+
+FAIT3:
+
+	;iload -2
+	push word ptr [bp-2]
+
+	;iconst 1
+	push word ptr 1
+
+	;iadd
+	pop bx
+	pop ax
+	add ax,bx
+	push ax
+
+	;istore -2
+	pop ax
+	mov word ptr [bp-2],ax
+
+	;goto FAIRE2
+	jmp FAIRE2
+
+FAIT2:
 
 	;queue
 	nop
